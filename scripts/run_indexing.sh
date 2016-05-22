d1adb5fd4c3c0fa3f548efe684102666f233425e
Dima Ilvokhin <r3tsky@gmail.com>
2016-05-22 11:55:08 +0300

    fix script

--- scripts/run_indexing.sh
+++ scripts/run_indexing.sh
@@ -8,4 +8,4 @@ threads=8
 
 src=~/crawler/3kk-5kk
 
-time ls $src/*out | xargs -I{} -P$threads bash -c "cat {} | $bin/run_indexer > {}.bin"
+time ls $src/*out | xargs -I{} -P$threads bash -c "cat {} | $bin/run_indexer {}.bin"
